6bc8ba8c571dde42846c10a312d706f7c4fdfa2f
akshithagouddonthi-ui <akshithagouddonthi@gmail.com>
2026-08-23 19:48:40 +0530

    data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,19 @@
+#include<<iostream>>
+#include<<string>>
+using namespace std;
+int main ()
+{
+  int age=19;
+float marks=14.5;
+char gender= 'F';
+double pi=3.14;
+bool passed='true';
+string name="akshitha";
+cout<<"my name:"<<age<<endl;
+cout<<"my marks:"<<marks<<endl;
+cout<<"my gender:"<<gender<<endl;
+cout<<"value:"<<pi<<endl;
+cout<<"thank god:"<<passed<<endl;
+cout<<"my name:"<<name<<endl;
+return 0;
+}
