8329eb786c784930de05268cb21c473345793695
akshithagouddonthi-ui <akshithagouddonthi@gmail.com>
2026-08-23 19:18:42 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include <iosteram>
+#include <string>
+using namespace std;
+int main()
+{
+  int age=12;
+float marks=12.5;
+char grade='A';
+string name="akshitha";
+cout<<"my age:"<<age<<endl;
+cout<<"my marks:"<<marks<<endl;
+cout<<"my grade:"<<grade<<endl;
+cout<<'my name:"<<name<<endl;
+return 0;
+}
