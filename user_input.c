edc11d87fbe25a42318c292348402d99fe465384
akshithagouddonthi-ui <akshithagouddonthi@gmail.com>
2026-08-23 21:36:49 +0530

    user_input.c

---
+++ user_input.c
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+int age;
+float marks;
+char grade;
+string name;
+cout<<"enter your age:";
+cin>>age;
+cout<<"enter your marks:";
+cin>>marks;
+cout<<"enter your grade:";
+cin>>grade;
+cout<<"enter your name:";
+cin>>name;
+return 0;
+}
