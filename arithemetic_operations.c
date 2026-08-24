e991abf2d808cfc131f1f2e62dd0d64d674573e1
akshithagouddonthi-ui <akshithagouddonthi@gmail.com>
2026-08-24 07:43:32 +0530

    arithemetic_operations.c

---
+++ arithemetic_operations.c
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main 
+{
+int a=3;
+int b=4;
+int sum=a+b;
+int difference=a-b;
+int product=a*b;
+int quotient=a/b;
+int reminder=a%b;
+cout<<"sum of the numbers:"<<sum<<endl;
+cout<<"difference of numbers:"<<difference<<endl;
+cout<<"product of numbers:"<<product<<endl;
+cout<<"quotient of numbers:"<<quotient<<endl;
+cout<<"reminder of numbers:"<<reminder<<endl;
+return 0;
+}
+  +
