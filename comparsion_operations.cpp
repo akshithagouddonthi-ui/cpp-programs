e58e5ecd276ea79f8b9755b1a3ace14b46173788
akshithagouddonthi-ui <akshithagouddonthi@gmail.com>
2026-08-25 07:07:45 +0530

    comparsion_operations.cpp

---
+++ comparsion_operations.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+cout<<"enter a number:";
+cin>>a;
+cout<<"enter a number:";
+cin>>b;
+cout<<"numbers are equal:"<<(a==b)<<endl;
+cout<<"numbers are not equal:"<<(a!=b)<<endl;
+cout<<"a is greater than b:"<<(a>b)<<endl;
+cout<<"a is less than b:"<<(a<b)<<endl;
+cout<<"a is greater than or equals to b:"<<(a>=b)<<endl;
+cout<<"a is less than or equals to b:"<<(a<=b)<<endl;
+return 0;
+}
+
+
+
+
+
